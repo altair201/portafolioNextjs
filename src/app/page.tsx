32e3ab6feae4036489870215988e517f1a5f8fa2
altair201 <wilsondavianpa1998@outlook.com>
2023-11-25 16:17:50 -0500

    update theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useAppSelector } from '@/redux/hooks'
 export default function Home() {
   const darkMode = useAppSelector((state) => state.DarkModeSlice.darkMode);
   return (
-    <main className={darkMode ? 'dark bg-zinc-900 containerDark  text-white' : 'h-screen'} >
+    <main className={darkMode ? 'dark bg-zinc-900 containerDark  text-white' : 'h-screen containerDark'} >
       <Landing />
     </main>
   )
